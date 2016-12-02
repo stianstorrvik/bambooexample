3369acaeb99c121fee5aec28f837975c1da6ce50
Stian Storrvik <stianstorrvik@gmail.com>
2016-12-02 18:07:06 +0100

    Add numberReturn to service

--- module1/src/main/java/com/storrvik/SomeClass.java
+++ module1/src/main/java/com/storrvik/SomeClass.java
@@ -3,6 +3,10 @@ package com.storrvik.module2;
 import com.storrvik.module1.SomeService;
 
 public class SomeClass {
+	
+	public int returnNumber() {
+		return new SomeService().returnNumber();
+	}
 	public boolean returnFalse() {
 		return new SomeService().returnFalse();
 	}
--- module2/src/main/java/com/storrvik/SomeService.java
+++ module2/src/main/java/com/storrvik/SomeService.java
@@ -1,6 +1,9 @@
 package com.storrvik.module1;
 
 public class SomeService {
+	public int returnNumber() {
+		return 1;
+	}
 	
 	public boolean returnFalse() {
 		return false;
