6c7d3a3cbd4b04adf732f41903e5d83a2fde7f81
Stian Storrvik <stianstorrvik@gmail.com>
2016-12-05 13:13:15 +0100

    Remove java files

--- module1/src/test/java/com/storrvik/SomeServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.storrvik.module1;
-
-public class SomeServiceTest {
-	
-	/** Add random tests here */
-	
-}
--- module2/src/test/java/com/storrvik/SomeClassTest.java
+++
@@ -1,7 +0,0 @@
-package com.storrvik.module2;
-
-public class SomeClassTest {
-	
-	/** Add some random tests here */
-	
-}
